c0efff875726f346dc339bd06e42a4fc4fece06b
Rajasekhar <prsr4u@gmail.com>
2019-08-28 19:29:26 +0530

    Aggrigation has a relation ship in inheritance it is a weak assosiaation between container and contained objects

---
+++ agrigation.py
@@ -0,0 +1,18 @@
+class Salary:
+    def __init__(self,pay,bonus):
+        self.pay=pay
+        self.bonus=bonus
+
+    def anual_sal(self):
+        return (self.pay*12)+self.bonus
+class Employee:
+    def __init__(self,name,age,salary):
+        self.name=name
+        self.age=age
+        self.obj_salary=salary
+
+    def total(self):
+        return self.obj_salary.anual_sal()
+salary=Salary(100000,1500)
+emp=Employee("sudheer",23,salary)
+print(emp.total())
